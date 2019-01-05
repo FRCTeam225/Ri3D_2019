e0ecf94b96bb82d376ebee6c5fbdf0112e4b06c8
Andrew <andrew@lobos.me>
2019-01-05 14:23:13 -0500

    Commit lib

---
+++ src/main/java/org/techfire225/lib/framework/AxisButton.java
@@ -0,0 +1,33 @@
+package org.techfire225.lib.framework;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+/**
+* @author Andrew
+*/
+public class AxisButton extends Button {
+	
+   Joystick joystick;
+   int axis;
+   double thresh;
+   
+   public AxisButton(Joystick joystick, int axis, double thresh) {
+       this.joystick = joystick;
+       this.axis = axis;
+       this.thresh = thresh;
+   }
+   
+   public boolean get() {
+       if ( thresh > 0 ) {
+           if ( joystick.getRawAxis(axis) > thresh )
+               return true;
+       } else if ( thresh < 0 ) {
+           if ( joystick.getRawAxis(axis) < thresh)
+               return true;
+       }
+       return false;
+   }
+   
+}
---
+++ src/main/java/org/techfire225/lib/framework/TFSubsystem.java
@@ -0,0 +1,24 @@
+package org.techfire225.lib.framework;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class TFSubsystem<Mode_T> extends Subsystem {	
+	
+	private volatile Mode_T currentMode;
+	protected volatile boolean onTarget = false;
+
+	public void setMode(Mode_T mode) {
+		currentMode = mode;
+		onTarget = false;
+	}
+	
+	public Mode_T getCurrentMode() {
+		return currentMode;
+	}
+	
+	public boolean atSetpoint() {
+		return onTarget;
+	}
+	
+	public abstract void refreshConstants();
+}
